64e58dd4ff44150e67c91afc9219ab00dbdbfd34
Neun <duythien0912@gmail.com>
2019-10-29 15:46:26 +0700

    [Thien] setup

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/key46/Documents/app/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/key46/Documents/pika_lab/background_locator-master_2/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/key46/Documents/app/flutter/bin/cache/artifacts/engine/ios"
